52cbf95d07355d37198cd0d443f96bfcf5610777
thinh <thinh>
2020-09-14 22:55:36 +0700

    fix modal not working

--- app.js
+++ app.js
@@ -20,6 +20,17 @@ const hbs = expressHandlebars.create({
     json: (context) => {
       return JSON.stringify(context);
     },
+    block: function (name) {
+        var blocks  = this._blocks,
+            content = blocks && blocks[name];
+
+        return content ? content.join('\n') : null;
+    },
+    contentFor: function (name, options) {
+        var blocks = this._blocks || (this._blocks = {}),
+            block  = blocks[name] || (blocks[name] = []);
+        block.push(options.fn(this));
+    }
   },
 });
 app.engine("hbs", hbs.engine);
