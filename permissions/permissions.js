f0c5208c72b6b1c632765c32b9fa17d0a25d25f5
Alirio <alirio.digital@gmail.com>
2020-12-04 14:55:30 -0500

    Google captcha for heroku

--- permissions/permissions.js
+++ permissions/permissions.js
@@ -3,23 +3,12 @@ const { isSchool, isAdmin } = require("../helpers/auth");
 const User = require("../models/User");
 
 
-// permissions.isSchool = (req, res, next) => {
-//   if(isSchool(req.user.school)) {
-//     next();
-//   } else {
-//     req.flash("error", "Sorry, you are not authorized to view this content");
-//     res.redirect("/courses");
-//   }
-// }
-
-
 permissions.canEditUser = (req, res, user) => {
 	if(req.user.school != user.school){
 		req.flash("error", "Sorry, you are not authorized to view this content");
     	res.redirect("/users");
 	}
 }
-
 
 permissions.canEditCourse = async (req, res, course) => {
 	if(!course.sessionUser) return;
